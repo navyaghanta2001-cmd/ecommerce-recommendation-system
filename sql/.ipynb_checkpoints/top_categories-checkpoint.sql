20e57bc13cc0b68b633f6bde5a6001a4be6c8189
Navya Ghanta <your-github-navyaghanta2001@gmail.com>
2026-08-01 10:52:56 -0400

    Rebuild git tracking with corrected gitignore rules

--- sql/.ipynb_checkpoints/top_categories-checkpoint.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-       product_category_name_english,
-       COUNT(DISTINCT order_id) AS num_orders,
-       SUM(total_item_value) AS total_revenue
-   FROM orders_master
-   GROUP BY product_category_name_english
-   ORDER BY total_revenue DESC
-   LIMIT 10;
--- sql/.ipynb_checkpoints/top_sellers-checkpoint.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-       seller_id,
-       COUNT(DISTINCT order_id) AS num_orders,
-       SUM(total_item_value) AS total_revenue
-   FROM orders_master
-   GROUP BY seller_id
-   ORDER BY total_revenue DESC
-   LIMIT 10;
